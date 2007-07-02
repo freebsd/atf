426c246d63f7a68366dedb21875d485eb0d52780
Julio Merino <jmmv@NetBSD.org>
2007-07-02 08:16:43 +0000

    Add an atf_check function to the shell library
    Introduce an atf_check function, mimicking GNU Autotest's AT_CHECK macro,
    that runs the given command and checks its exit code and what it writes
    to its stdout and stderr channels.  Based on past experience using
    Autotest, this is extremely useful.
    
    Also add a preliminary test program for atf-compile based on this function
    used to demonstrate its utility.  The test itself is still quite incomplete
    though.
    
    Monotone-Parent: f6b47174f4fa8569f461991f8bfb03b7247eeee7
    Monotone-Revision: 97c5e5f3c3103ab4677de94fa35476a8854d1681
    
    Monotone-Author: jmmv@NetBSD.org
    Monotone-Date: 2007-07-02T08:16:43
    Monotone-Branch: org.NetBSD.atf.src

---
+++ tests/atf/tools/t_atf_compile.sh
@@ -0,0 +1,81 @@
+#
+# Automated Testing Framework (atf)
+#
+# Copyright (c) 2007 The NetBSD Foundation, Inc.
+# All rights reserved.
+#
+# This code is derived from software contributed to The NetBSD Foundation
+# by Julio M. Merino Vidal, developed as part of Google's Summer of Code
+# 2007 program.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this
+#    software must display the following acknowledgement:
+#        This product includes software developed by the NetBSD
+#        Foundation, Inc. and its contributors.
+# 4. Neither the name of The NetBSD Foundation nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND
+# CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# TODO: This test program is probably incomplete.  It was first added
+# to demonstrate the utility of the atf_check function.  Inspect its
+# current status and complete it.
+
+tc_includes_head()
+{
+    atf_set "descr" "Tests that the resulting file includes the correct" \
+                    "shell subroutines"
+}
+tc_includes_body()
+{
+    cat >tp_test.sh <<EOF
+# This is a sample test program.
+EOF
+    atf_check 'atf-compile -o tp_test tp_test.sh' 0
+    atf_check 'grep ^\..*/atf.init.subr tp_test' 1
+    atf_check 'grep ^\..*/atf.header.subr tp_test' 0 ignore
+    atf_check 'grep ^\..*/atf.footer.subr tp_test' 0 ignore
+    atf_pass
+}
+
+tc_oflag_head()
+{
+    atf_set "descr" "Tests that the -o flag works"
+}
+tc_oflag_body()
+{
+    atf_check 'touch tp_foo.sh'
+    atf_check 'atf-compile tp_foo.sh' 0 stdout
+    atf_check 'test -f tp_foo' 1
+    atf_check 'atf-compile -o tp_foo tp_foo.sh' 0
+    atf_check 'test -f tp_foo' 0
+    atf_check 'cmp stdout tp_foo' 0 ignore
+    atf_pass
+}
+
+atf_init_test_cases()
+{
+    atf_add_test_case tc_oflag
+    atf_add_test_case tc_includes
+}
---
+++ tests/bootstrap/h_tp_atf_check_sh.sh
@@ -0,0 +1,303 @@
+#
+# Automated Testing Framework (atf)
+#
+# Copyright (c) 2007 The NetBSD Foundation, Inc.
+# All rights reserved.
+#
+# This code is derived from software contributed to The NetBSD Foundation
+# by Julio M. Merino Vidal, developed as part of Google's Summer of Code
+# 2007 program.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this
+#    software must display the following acknowledgement:
+#        This product includes software developed by the NetBSD
+#        Foundation, Inc. and its contributors.
+# 4. Neither the name of The NetBSD Foundation nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND
+# CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+tc_exitcode_0_0_head()
+{
+    atf_set "descr" "Runs a program that returns true and expects true"
+}
+tc_exitcode_0_0_body()
+{
+    atf_check 'true' 0
+    atf_pass
+}
+
+tc_exitcode_0_1_head()
+{
+    atf_set "descr" "Runs a program that returns true and expects false"
+}
+tc_exitcode_0_1_body()
+{
+    atf_check 'true' 1
+    atf_pass
+}
+
+tc_exitcode_1_0_head()
+{
+    atf_set "descr" "Runs a program that returns false and expects true"
+}
+tc_exitcode_1_0_body()
+{
+    atf_check 'false' 0
+    atf_pass
+}
+
+tc_exitcode_1_1_head()
+{
+    atf_set "descr" "Runs a program that returns false and expects false"
+}
+tc_exitcode_1_1_body()
+{
+    atf_check 'false' 1
+    atf_pass
+}
+
+tc_stdout_expout_pass_head()
+{
+    atf_set "descr" "Runs a program with stdout set to expout and passes"
+}
+tc_stdout_expout_pass_body()
+{
+    echo foo >expout
+    atf_check 'echo foo' 0 expout null
+    atf_pass
+}
+
+tc_stdout_expout_fail_head()
+{
+    atf_set "descr" "Runs a program with stdout set to expout and fails"
+}
+tc_stdout_expout_fail_body()
+{
+    echo foo >expout
+    atf_check 'echo bar' 0 expout null
+    atf_pass
+}
+
+tc_stdout_ignore_empty_head()
+{
+    atf_set "descr" "Runs a program with stdout set to ignore and" \
+                    "writes nothing"
+}
+tc_stdout_ignore_empty_body()
+{
+    atf_check 'true' 0 ignore null
+    atf_pass
+}
+
+tc_stdout_ignore_sth_head()
+{
+    atf_set "descr" "Runs a program with stdout set to ignore and" \
+                    "writes something"
+}
+tc_stdout_ignore_sth_body()
+{
+    atf_check 'echo foo' 0 ignore null
+    atf_pass
+}
+
+tc_stdout_null_empty_head()
+{
+    atf_set "descr" "Runs a program with stdout set to null and" \
+                    "writes nothing"
+}
+tc_stdout_null_empty_body()
+{
+    atf_check 'true' 0 null null
+    atf_pass
+}
+
+tc_stdout_null_sth_head()
+{
+    atf_set "descr" "Runs a program with stdout set to null and" \
+                    "writes something"
+}
+tc_stdout_null_sth_body()
+{
+    atf_check 'echo foo' 0 null null
+    atf_pass
+}
+
+tc_stdout_stdout_written_head()
+{
+    atf_set "descr" "Runs a program with stdout set to stdout and" \
+                    "writes something"
+}
+tc_stdout_stdout_written_body()
+{
+    atf_check 'echo foo' 0 stdout null
+    echo foo >aux
+    cmp -s stdout aux || atf_fail "Test failed"
+    atf_pass
+}
+
+tc_stdout_stdout_noclobber_head()
+{
+    atf_set "descr" "Runs a program multiple times with different stdout" \
+                    "modes and ensures that they do not overwrite an old" \
+                    "stdout file"
+}
+tc_stdout_stdout_noclobber_body()
+{
+    echo bar >stdout
+
+    echo foo >expout
+    atf_check 'echo foo' 0 expout null
+    atf_check 'echo foo' 0 ignore null
+    atf_check 'true' 0 null null
+
+    echo bar >aux
+    cmp -s stdout aux || atf_fail "Test failed"
+    atf_pass
+}
+
+tc_stderr_experr_pass_head()
+{
+    atf_set "descr" "Runs a program with stderr set to experr and passes"
+}
+tc_stderr_experr_pass_body()
+{
+    echo foo >experr
+    atf_check 'echo foo 1>&2' 0 null experr
+    atf_pass
+}
+
+tc_stderr_experr_fail_head()
+{
+    atf_set "descr" "Runs a program with stderr set to experr and fails"
+}
+tc_stderr_experr_fail_body()
+{
+    echo foo >experr
+    atf_check 'echo bar 1>&2' 0 null experr
+    atf_pass
+}
+
+tc_stderr_ignore_empty_head()
+{
+    atf_set "descr" "Runs a program with stderr set to ignore and" \
+                    "writes nothing"
+}
+tc_stderr_ignore_empty_body()
+{
+    atf_check 'true 1>&2' 0 null ignore
+    atf_pass
+}
+
+tc_stderr_ignore_sth_head()
+{
+    atf_set "descr" "Runs a program with stderr set to ignore and" \
+                    "writes something"
+}
+tc_stderr_ignore_sth_body()
+{
+    atf_check 'echo foo 1>&2' 0 null ignore
+    atf_pass
+}
+
+tc_stderr_null_empty_head()
+{
+    atf_set "descr" "Runs a program with stderr set to null and" \
+                    "writes nothing"
+}
+tc_stderr_null_empty_body()
+{
+    atf_check 'true 1>&2' 0 null null
+    atf_pass
+}
+
+tc_stderr_null_sth_head()
+{
+    atf_set "descr" "Runs a program with stderr set to null and" \
+                    "writes something"
+}
+tc_stderr_null_sth_body()
+{
+    atf_check 'echo foo 1>&2' 0 null null
+    atf_pass
+}
+
+tc_stderr_stderr_written_head()
+{
+    atf_set "descr" "Runs a program with stderr set to stderr and" \
+                    "writes something"
+}
+tc_stderr_stderr_written_body()
+{
+    atf_check 'echo foo 1>&2' 0 null stderr
+    echo foo >aux
+    cmp -s stderr aux || atf_fail "Test failed"
+    atf_pass
+}
+
+tc_stderr_stderr_noclobber_head()
+{
+    atf_set "descr" "Runs a program multiple times with different stderr" \
+                    "modes and ensures that they do not overwrite an old" \
+                    "stderr file"
+}
+tc_stderr_stderr_noclobber_body()
+{
+    echo bar >stderr
+
+    echo foo >experr
+    atf_check 'echo foo 1>&2' 0 null experr
+    atf_check 'echo foo 1>&2' 0 null ignore
+    atf_check 'true 1>&2' 0 null null
+
+    echo bar >aux
+    cmp -s stderr aux || atf_fail "Test failed"
+    atf_pass
+}
+
+atf_init_test_cases()
+{
+    atf_add_test_case tc_exitcode_0_0
+    atf_add_test_case tc_exitcode_0_1
+    atf_add_test_case tc_exitcode_1_0
+    atf_add_test_case tc_exitcode_1_1
+
+    atf_add_test_case tc_stdout_expout_pass
+    atf_add_test_case tc_stdout_expout_fail
+    atf_add_test_case tc_stdout_ignore_empty
+    atf_add_test_case tc_stdout_ignore_sth
+    atf_add_test_case tc_stdout_null_empty
+    atf_add_test_case tc_stdout_null_sth
+    atf_add_test_case tc_stdout_stdout_written
+    atf_add_test_case tc_stdout_stdout_noclobber
+
+    atf_add_test_case tc_stderr_experr_pass
+    atf_add_test_case tc_stderr_experr_fail
+    atf_add_test_case tc_stderr_ignore_empty
+    atf_add_test_case tc_stderr_ignore_sth
+    atf_add_test_case tc_stderr_null_empty
+    atf_add_test_case tc_stderr_null_sth
+    atf_add_test_case tc_stderr_stderr_written
+    atf_add_test_case tc_stderr_stderr_noclobber
+}
