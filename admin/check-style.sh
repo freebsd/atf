cab9a3817a4d86023e0c34e70a8d3643a8b482e7
Julio Merino <jmmv@NetBSD.org>
2007-08-12 18:53:49 +0000

    Clean up the license text in the source's headers
    Remove the "This code is derived from..." sentence from the license text
    in all source files:
    
    - This is not really part of the license.  Someone reusing this code has
      to reproduce the Copyright sentence, but not that one.  However, keeping
      the line only drives to confusion.
    
    - Putting names in source files is asking for trouble, because... at which
      point new contributors are allowed to add their name?  What happens to
      the names if the code is severely refactored by another person?  (For
      new contributors I'm going to *require* them to assign the copyright to
      the TNF as I did, which will prevent this too.)
    
    - Similary, the sentence noted that the code was initially done as part
      of Google Summer of Code 2007.  But it is not possible to know which
      parts were done during the program and which outside it.  I'm adding
      another note about this program in the AUTHORS file, though.
    
    - Credit is already given where it should be: in the AUTHORS file and in
      the repository logs.
    
    Monotone-Parent: a15fd0ad7a182ef6bb176f275cc942e4f969d12f
    Monotone-Revision: 78a61e2777fe3d9372b632db1dfb959c9f50925d
    
    Monotone-Author: jmmv@NetBSD.org
    Monotone-Date: 2007-08-12T18:53:49
    Monotone-Branch: org.NetBSD.atf.src

--- admin/check-style.sh
+++ admin/check-style.sh
@@ -5,10 +5,6 @@
 # Copyright (c) 2007 The NetBSD Foundation, Inc.
 # All rights reserved.
 #
-# This code is derived from software contributed to The NetBSD Foundation
-# by Julio M. Merino Vidal, developed as part of Google's Summer of Code
-# 2007 program.
-#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
