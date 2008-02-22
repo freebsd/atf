5d4c19839d3280d98b3166f2e533d50ee8f2c047
Julio Merino <jmmv@NetBSD.org>
2008-02-22 15:04:11 +0000

    Add the atf_map type
    Add an extremely simple map type based on a list.  This is surely not
    efficient, but our maps are very small and thus this is not a big problem
    right now.
    
    Monotone-Parent: 2d466d01c1b999b4f275de7a52557375fbfd95ce
    Monotone-Revision: 6ab7420a1febe5a8984bb16645a225f1f90bf5a0
    
    Monotone-Author: jmmv@NetBSD.org
    Monotone-Date: 2008-02-22T15:04:11

---
+++ tests/atf/atf-c/t_map.c
@@ -0,0 +1,143 @@
+/*
+ * Automated Testing Framework (atf)
+ *
+ * Copyright (c) 2008 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgement:
+ *        This product includes software developed by the NetBSD
+ *        Foundation, Inc. and its contributors.
+ * 4. Neither the name of The NetBSD Foundation nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND
+ * CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <atf-c.h>
+
+#include "atf-c/map.h"
+
+#define CE(stm) ATF_CHECK(!atf_is_error(stm))
+
+/* ---------------------------------------------------------------------
+ * Tests for the "atf_map" type.
+ * --------------------------------------------------------------------- */
+
+/*
+ * Constructors and destructors.
+ */
+
+ATF_TC(map_init);
+ATF_TC_HEAD(map_init, tc)
+{
+    atf_tc_set_var(tc, "descr", "Checks the atf_map_init function");
+}
+ATF_TC_BODY(map_init, tc)
+{
+    atf_map_t map;
+
+    CE(atf_map_init(&map));
+    ATF_CHECK_EQUAL(atf_map_size(&map), 0);
+    atf_map_fini(&map);
+}
+
+/*
+ * Getters.
+ */
+
+ATF_TC(find_c);
+ATF_TC_HEAD(find_c, tc)
+{
+    atf_tc_set_var(tc, "descr", "Checks the atf_map_find_c function");
+}
+ATF_TC_BODY(find_c, tc)
+{
+    atf_map_t map;
+    char val1[] = "V1";
+    char val2[] = "V2";
+    atf_map_citer_t iter;
+
+    CE(atf_map_init(&map));
+    CE(atf_map_insert(&map, "K1", val1, false));
+    CE(atf_map_insert(&map, "K2", val2, false));
+
+    iter = atf_map_find_c(&map, "K0");
+    ATF_CHECK(atf_equal_map_citer_map_citer(iter, atf_map_end_c(&map)));
+
+    iter = atf_map_find_c(&map, "K1");
+    ATF_CHECK(!atf_equal_map_citer_map_citer(iter, atf_map_end_c(&map)));
+    ATF_CHECK(strcmp(atf_map_citer_data(iter), "V1") == 0);
+
+    iter = atf_map_find_c(&map, "K2");
+    ATF_CHECK(!atf_equal_map_citer_map_citer(iter, atf_map_end_c(&map)));
+    ATF_CHECK(strcmp(atf_map_citer_data(iter), "V2") == 0);
+
+    atf_map_fini(&map);
+}
+
+/*
+ * Modifiers.
+ */
+
+ATF_TC(map_insert);
+ATF_TC_HEAD(map_insert, tc)
+{
+    atf_tc_set_var(tc, "descr", "Checks the atf_map_insert function");
+}
+ATF_TC_BODY(map_insert, tc)
+{
+    atf_map_t map;
+    char buf[] = "Test string";
+
+    CE(atf_map_init(&map));
+    ATF_CHECK_EQUAL(atf_map_size(&map), 0);
+    CE(atf_map_insert(&map, "K1", buf, false));
+    ATF_CHECK_EQUAL(atf_map_size(&map), 1);
+    CE(atf_map_insert(&map, "K2", buf, false));
+    ATF_CHECK_EQUAL(atf_map_size(&map), 2);
+    CE(atf_map_insert(&map, "K3", buf, false));
+    ATF_CHECK_EQUAL(atf_map_size(&map), 3);
+    atf_map_fini(&map);
+}
+
+/* ---------------------------------------------------------------------
+ * Main.
+ * --------------------------------------------------------------------- */
+
+ATF_TP_ADD_TCS(tp)
+{
+    /* Constructors and destructors. */
+    ATF_TP_ADD_TC(tp, map_init);
+
+    /* Getters. */
+    ATF_TP_ADD_TC(tp, find_c);
+
+    /* Modifiers. */
+    ATF_TP_ADD_TC(tp, map_insert);
+
+    return atf_no_error();
+}
