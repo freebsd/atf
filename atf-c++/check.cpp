308b61e1dd0e409c61dd94deda837bddeac4f216
Lukasz Strzygowski <qx89l4@gmail.com>
2008-07-08 17:04:57 +0000

    Add missing check.cpp
    Add missing check.cpp containing C++ api for atf_check.
    
    Monotone-Parent: 2a609e5f4208a8d92f7fc24f6735960f2a518545
    Monotone-Revision: 3cd14eb5c8182d127c5fbaf3e84a8fd3e3b42d85
    
    Monotone-Author: qx89l4@gmail.com
    Monotone-Date: 2008-07-08T17:04:57

---
+++ atf-c++/check.cpp
@@ -0,0 +1,68 @@
+//
+// Automated Testing Framework (atf)
+//
+// Copyright (c) 2007, 2008 The NetBSD Foundation, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND
+// CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include "atf-c++/check.hpp"
+#include "atf-c++/exceptions.hpp"
+
+namespace impl = atf::check;
+#define IMPL_NAME "atf::check"
+
+impl::check_result::check_result(const std::string &command)
+{
+    atf_error_t err = atf_check_exec(&m_result, command.c_str());
+
+    if (atf_is_error(err))
+        throw_atf_error(err);
+}
+
+impl::check_result::~check_result()
+{
+    atf_check_result_fini(&m_result);
+}
+
+int
+impl::check_result::status()
+    const
+{
+    return atf_check_result_status(&m_result);
+}
+
+const atf::fs::path
+impl::check_result::stdout_path()
+    const
+{
+    return atf_check_result_stdout(&m_result);
+}
+
+const atf::fs::path
+impl::check_result::stderr_path()
+        const
+    {
+    return atf_check_result_stderr(&m_result);
+}
