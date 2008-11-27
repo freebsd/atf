0282de0e923fd80aad255ad62216b58dbfe715b7
Julio Merino <jmmv@NetBSD.org>
2008-11-27 20:12:33 +0000

    Add tests for all ATF_REQUIRE_* C macros
    Complete the tests for the ATF_REQUIRE_* C macros by adding some missing
    ones.  ATF_CHECK_* tests will be next.
    
    Monotone-Parent: e00fda1028e745ab8b6094a52865b54ee1c1e6c0
    Monotone-Revision: bdac1e674e67e3004dcc6f8ecfe9eb5de160aff4
    
    Monotone-Author: jmmv@NetBSD.org
    Monotone-Date: 2008-11-27T20:12:33

--- tests/atf/atf-c/t_macros.c
+++ tests/atf/atf-c/t_macros.c
@@ -134,9 +134,7 @@ match_reason(const atf_tcr_t *tcr, const char *regex, ...)
  * Helper test cases.
  * --------------------------------------------------------------------- */
 
-#define H_REQUIRE_HEAD_NAME(id) ATF_TC_HEAD_NAME(h_require_ ## id)
-#define H_REQUIRE_BODY_NAME(id) ATF_TC_BODY_NAME(h_require_ ## id)
-#define H_REQUIRE(id, condition) \
+#define H_REQUIRE_DEF(id, macro) \
     ATF_TC_HEAD(h_require_ ## id, tc) \
     { \
         atf_tc_set_md_var(tc, "descr", "Helper test case"); \
@@ -144,57 +142,86 @@ match_reason(const atf_tcr_t *tcr, const char *regex, ...)
     ATF_TC_BODY(h_require_ ## id, tc) \
     { \
         create_ctl_file(tc, "before"); \
-        ATF_REQUIRE(condition); \
+        macro; \
         create_ctl_file(tc, "after"); \
     }
+
+#define H_REQUIRE_HEAD_NAME(id) ATF_TC_HEAD_NAME(h_require_ ## id)
+#define H_REQUIRE_BODY_NAME(id) ATF_TC_BODY_NAME(h_require_ ## id)
+#define H_REQUIRE(id, condition) H_REQUIRE_DEF(id, ATF_REQUIRE(condition))
+
+#define H_REQUIRE_MSG_HEAD_NAME(id) ATF_TC_HEAD_NAME(h_require_msg_ ## id)
+#define H_REQUIRE_MSG_BODY_NAME(id) ATF_TC_BODY_NAME(h_require_msg_ ## id)
+#define H_REQUIRE_MSG(id, condition, msg) \
+    H_REQUIRE_DEF(msg_ ## id, ATF_REQUIRE_MSG(condition, msg))
 
 #define H_REQUIRE_EQ_HEAD_NAME(id) ATF_TC_HEAD_NAME(h_require_eq_ ## id)
 #define H_REQUIRE_EQ_BODY_NAME(id) ATF_TC_BODY_NAME(h_require_eq_ ## id)
 #define H_REQUIRE_EQ(id, v1, v2) \
-    ATF_TC_HEAD(h_require_eq_ ## id, tc) \
-    { \
-        atf_tc_set_md_var(tc, "descr", "Helper test case"); \
-    } \
-    ATF_TC_BODY(h_require_eq_ ## id, tc) \
-    { \
-        create_ctl_file(tc, "before"); \
-        ATF_REQUIRE_EQ(v1, v2); \
-        create_ctl_file(tc, "after"); \
-    }
+    H_REQUIRE_DEF(eq_ ## id, ATF_REQUIRE_EQ(v1, v2))
+
+#define H_REQUIRE_STREQ_HEAD_NAME(id) ATF_TC_HEAD_NAME(h_require_streq_ ## id)
+#define H_REQUIRE_STREQ_BODY_NAME(id) ATF_TC_BODY_NAME(h_require_streq_ ## id)
+#define H_REQUIRE_STREQ(id, v1, v2) \
+    H_REQUIRE_DEF(streq_ ## id, ATF_REQUIRE_STREQ(v1, v2))
+
+#define H_REQUIRE_EQ_MSG_HEAD_NAME(id) \
+    ATF_TC_HEAD_NAME(h_require_eq_msg_ ## id)
+#define H_REQUIRE_EQ_MSG_BODY_NAME(id) \
+    ATF_TC_BODY_NAME(h_require_eq_msg_ ## id)
+#define H_REQUIRE_EQ_MSG(id, v1, v2, msg) \
+    H_REQUIRE_DEF(eq_msg_ ## id, ATF_REQUIRE_EQ_MSG(v1, v2, msg))
+
+#define H_REQUIRE_STREQ_MSG_HEAD_NAME(id) \
+    ATF_TC_HEAD_NAME(h_require_streq_msg_ ## id)
+#define H_REQUIRE_STREQ_MSG_BODY_NAME(id) \
+    ATF_TC_BODY_NAME(h_require_streq_msg_ ## id)
+#define H_REQUIRE_STREQ_MSG(id, v1, v2, msg) \
+    H_REQUIRE_DEF(streq_msg_ ## id, ATF_REQUIRE_STREQ_MSG(v1, v2, msg))
 
 /* ---------------------------------------------------------------------
- * Test cases for the macros.
+ * Test cases for the ATF_REQUIRE and ATF_REQUIRE_MSG macros.
  * --------------------------------------------------------------------- */
 
-H_REQUIRE(false, 0);
-H_REQUIRE(true, 1);
+H_REQUIRE(0, 0);
+H_REQUIRE(1, 1);
+H_REQUIRE_MSG(0, 0, "expected a false value");
+H_REQUIRE_MSG(1, 1, "expected a true value");
 
 ATF_TC(require);
 ATF_TC_HEAD(require, tc)
 {
-    atf_tc_set_md_var(tc, "descr", "Tests the ATF_REQUIRE macro");
+    atf_tc_set_md_var(tc, "descr", "Tests the ATF_REQUIRE and "
+                      "ATF_REQUIRE_MSG macros");
 }
 ATF_TC_BODY(require, tc)
 {
     struct test {
         void (*head)(atf_tc_t *);
         void (*body)(const atf_tc_t *);
-        const char *cond;
+        bool value;
+        const char *msg;
         bool ok;
     } *t, tests[] = {
-        { H_REQUIRE_HEAD_NAME(false), H_REQUIRE_BODY_NAME(false), "0", false },
-        { H_REQUIRE_HEAD_NAME(true), H_REQUIRE_BODY_NAME(true), "1", true },
-        { NULL, NULL, NULL, false }
+        { H_REQUIRE_HEAD_NAME(0), H_REQUIRE_BODY_NAME(0), 0,
+          "0 not met", false },
+        { H_REQUIRE_HEAD_NAME(1), H_REQUIRE_BODY_NAME(1), 1,
+          "1 not met", true },
+        { H_REQUIRE_MSG_HEAD_NAME(0), H_REQUIRE_MSG_BODY_NAME(0), 0,
+          "expected a false value", false },
+        { H_REQUIRE_MSG_HEAD_NAME(1), H_REQUIRE_MSG_BODY_NAME(1), 1,
+          "expected a true value", true },
+        { NULL, NULL, false, NULL, false }
     };
 
-    for (t = &tests[0]; t->cond != NULL; t++) {
+    for (t = &tests[0]; t->head != NULL; t++) {
         atf_map_t config;
         atf_tc_t tcaux;
         atf_tcr_t tcr;
 
         init_config(&config);
 
-        printf("Checking with a %s value\n", t->cond);
+        printf("Checking with a %d value\n", t->value);
 
         CE(atf_tc_init(&tcaux, "h_require", t->head, t->body, NULL, &config));
         run_here(&tcaux, &tcr);
@@ -207,7 +234,7 @@ ATF_TC_BODY(require, tc)
         } else {
             ATF_REQUIRE(atf_tcr_get_state(&tcr) == atf_tcr_failed_state);
             ATF_REQUIRE(!exists("after"));
-            ATF_REQUIRE(match_reason(&tcr, "^Line [0-9]+: %s not met$", t->cond));
+            ATF_REQUIRE(match_reason(&tcr, "^Line [0-9]+: %s$", t->msg));
         }
 
         atf_tcr_fini(&tcr);
@@ -219,44 +246,31 @@ ATF_TC_BODY(require, tc)
     }
 }
 
-H_REQUIRE_EQ(1_1, 1, 1);
-H_REQUIRE_EQ(1_2, 1, 2);
-H_REQUIRE_EQ(2_1, 2, 1);
-H_REQUIRE_EQ(2_2, 2, 2);
+/* ---------------------------------------------------------------------
+ * Test cases for the ATF_REQUIRE_*EQ_ macros.
+ * --------------------------------------------------------------------- */
 
-ATF_TC(require_eq);
-ATF_TC_HEAD(require_eq, tc)
-{
-    atf_tc_set_md_var(tc, "descr", "Tests the ATF_REQUIRE_EQ macro");
-}
-ATF_TC_BODY(require_eq, tc)
-{
-    struct test {
-        void (*head)(atf_tc_t *);
-        void (*body)(const atf_tc_t *);
-        const char *v1;
-        const char *v2;
-        bool ok;
-    } *t, tests[] = {
-        { H_REQUIRE_EQ_HEAD_NAME(1_1), H_REQUIRE_EQ_BODY_NAME(1_1),
-          "1", "1", true },
-        { H_REQUIRE_EQ_HEAD_NAME(1_2), H_REQUIRE_EQ_BODY_NAME(1_2),
-          "1", "2", false },
-        { H_REQUIRE_EQ_HEAD_NAME(2_1), H_REQUIRE_EQ_BODY_NAME(2_1),
-          "2", "1", false },
-        { H_REQUIRE_EQ_HEAD_NAME(2_2), H_REQUIRE_EQ_BODY_NAME(2_2),
-          "2", "2", true },
-        { NULL, NULL, NULL, NULL, false }
-    };
+struct require_eq_test {
+    void (*head)(atf_tc_t *);
+    void (*body)(const atf_tc_t *);
+    const char *v1;
+    const char *v2;
+    const char *msg;
+    bool ok;
+};
 
-    for (t = &tests[0]; t->v1 != NULL; t++) {
+static
+void
+do_require_eq_tests(const struct require_eq_test *tests)
+{
+    const struct require_eq_test *t;
+
+    for (t = &tests[0]; t->head != NULL; t++) {
         atf_map_t config;
         atf_tc_t tcaux;
         atf_tcr_t tcr;
 
         init_config(&config);
-        CE(atf_map_insert(&config, "v1", strdup(t->v1), true));
-        CE(atf_map_insert(&config, "v2", strdup(t->v2), true));
 
         printf("Checking with %s, %s and expecting %s\n", t->v1, t->v2,
                t->ok ? "true" : "false");
@@ -272,8 +286,7 @@ ATF_TC_BODY(require_eq, tc)
         } else {
             ATF_REQUIRE(atf_tcr_get_state(&tcr) == atf_tcr_failed_state);
             ATF_REQUIRE(!exists("after"));
-            ATF_REQUIRE(match_reason(&tcr, "^Line [0-9]+: %s != %s$",
-                                     t->v1, t->v2));
+            ATF_REQUIRE(match_reason(&tcr, "^Line [0-9]+: %s$", t->msg));
         }
 
         atf_tcr_fini(&tcr);
@@ -285,6 +298,88 @@ ATF_TC_BODY(require_eq, tc)
     }
 }
 
+H_REQUIRE_EQ(1_1, 1, 1);
+H_REQUIRE_EQ(1_2, 1, 2);
+H_REQUIRE_EQ(2_1, 2, 1);
+H_REQUIRE_EQ(2_2, 2, 2);
+H_REQUIRE_EQ_MSG(1_1, 1, 1, "1 does not match 1");
+H_REQUIRE_EQ_MSG(1_2, 1, 2, "1 does not match 2");
+H_REQUIRE_EQ_MSG(2_1, 2, 1, "2 does not match 1");
+H_REQUIRE_EQ_MSG(2_2, 2, 2, "2 does not match 2");
+
+ATF_TC(require_eq);
+ATF_TC_HEAD(require_eq, tc)
+{
+    atf_tc_set_md_var(tc, "descr", "Tests the ATF_REQUIRE_EQ and "
+                      "ATF_REQUIRE_EQ_MSG macros");
+}
+ATF_TC_BODY(require_eq, tc)
+{
+    struct require_eq_test tests[] = {
+        { H_REQUIRE_EQ_HEAD_NAME(1_1), H_REQUIRE_EQ_BODY_NAME(1_1),
+          "1", "1", "1 != 1", true },
+        { H_REQUIRE_EQ_HEAD_NAME(1_2), H_REQUIRE_EQ_BODY_NAME(1_2),
+          "1", "2", "1 != 2", false },
+        { H_REQUIRE_EQ_HEAD_NAME(2_1), H_REQUIRE_EQ_BODY_NAME(2_1),
+          "2", "1", "2 != 1", false },
+        { H_REQUIRE_EQ_HEAD_NAME(2_2), H_REQUIRE_EQ_BODY_NAME(2_2),
+          "2", "2", "2 != 2", true },
+        { H_REQUIRE_EQ_MSG_HEAD_NAME(1_1), H_REQUIRE_EQ_MSG_BODY_NAME(1_1),
+          "1", "1", "1 != 1: 1 does not match 1", true },
+        { H_REQUIRE_EQ_MSG_HEAD_NAME(1_2), H_REQUIRE_EQ_MSG_BODY_NAME(1_2),
+          "1", "2", "1 != 2: 1 does not match 2", false },
+        { H_REQUIRE_EQ_MSG_HEAD_NAME(2_1), H_REQUIRE_EQ_MSG_BODY_NAME(2_1),
+          "2", "1", "2 != 1: 2 does not match 1", false },
+        { H_REQUIRE_EQ_MSG_HEAD_NAME(2_2), H_REQUIRE_EQ_MSG_BODY_NAME(2_2),
+          "2", "2", "2 != 2: 2 does not match 2", true },
+        { NULL, NULL, 0, 0, "", false }
+    };
+    do_require_eq_tests(tests);
+}
+
+H_REQUIRE_STREQ(1_1, "1", "1");
+H_REQUIRE_STREQ(1_2, "1", "2");
+H_REQUIRE_STREQ(2_1, "2", "1");
+H_REQUIRE_STREQ(2_2, "2", "2");
+H_REQUIRE_STREQ_MSG(1_1, "1", "1", "1 does not match 1");
+H_REQUIRE_STREQ_MSG(1_2, "1", "2", "1 does not match 2");
+H_REQUIRE_STREQ_MSG(2_1, "2", "1", "2 does not match 1");
+H_REQUIRE_STREQ_MSG(2_2, "2", "2", "2 does not match 2");
+
+ATF_TC(require_streq);
+ATF_TC_HEAD(require_streq, tc)
+{
+    atf_tc_set_md_var(tc, "descr", "Tests the ATF_REQUIRE_STREQ and "
+                      "ATF_REQUIRE_STREQ_MSG macros");
+}
+ATF_TC_BODY(require_streq, tc)
+{
+    struct require_eq_test tests[] = {
+        { H_REQUIRE_STREQ_HEAD_NAME(1_1), H_REQUIRE_STREQ_BODY_NAME(1_1),
+          "1", "1", "\"1\" != \"1\"", true },
+        { H_REQUIRE_STREQ_HEAD_NAME(1_2), H_REQUIRE_STREQ_BODY_NAME(1_2),
+          "1", "2", "\"1\" != \"2\"", false },
+        { H_REQUIRE_STREQ_HEAD_NAME(2_1), H_REQUIRE_STREQ_BODY_NAME(2_1),
+          "2", "1", "\"2\" != \"1\"", false },
+        { H_REQUIRE_STREQ_HEAD_NAME(2_2), H_REQUIRE_STREQ_BODY_NAME(2_2),
+          "2", "2", "\"2\" != \"2\"", true },
+        { H_REQUIRE_STREQ_MSG_HEAD_NAME(1_1),
+          H_REQUIRE_STREQ_MSG_BODY_NAME(1_1),
+          "1", "1", "\"1\" != \"1\": 1 does not match 1", true },
+        { H_REQUIRE_STREQ_MSG_HEAD_NAME(1_2),
+          H_REQUIRE_STREQ_MSG_BODY_NAME(1_2),
+          "1", "2", "\"1\" != \"2\": 1 does not match 2", false },
+        { H_REQUIRE_STREQ_MSG_HEAD_NAME(2_1),
+          H_REQUIRE_STREQ_MSG_BODY_NAME(2_1),
+          "2", "1", "\"2\" != \"1\": 2 does not match 1", false },
+        { H_REQUIRE_STREQ_MSG_HEAD_NAME(2_2),
+          H_REQUIRE_STREQ_MSG_BODY_NAME(2_2),
+          "2", "2", "\"2\" != \"2\": 2 does not match 2", true },
+        { NULL, NULL, 0, 0, "", false }
+    };
+    do_require_eq_tests(tests);
+}
+
 /* ---------------------------------------------------------------------
  * Main.
  * --------------------------------------------------------------------- */
@@ -293,6 +388,7 @@ ATF_TP_ADD_TCS(tp)
 {
     ATF_TP_ADD_TC(tp, require);
     ATF_TP_ADD_TC(tp, require_eq);
+    ATF_TP_ADD_TC(tp, require_streq);
 
     return atf_no_error();
 }
