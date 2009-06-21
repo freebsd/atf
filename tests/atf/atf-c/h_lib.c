9d49afa7fa3b0aada09621fbca85ec5ab18bc11a
Julio Merino <jmmv@NetBSD.org>
2009-06-21 17:06:38 +0000

    Delay failure in build_check_c_o
    In case of failure in the auxiliary build_check_c_o function, do not exit
    the test case before freeing any objects allocated by the function to fix
    several (3) leaks.
    
    Monotone-Parent: 486bc645b952edcc824df063a0508da1fea0b926
    Monotone-Revision: 116a9223b420a6ffb47b2ec36a770a345b35df43
    
    Monotone-Author: jmmv@NetBSD.org
    Monotone-Date: 2009-06-21T17:06:38
    Monotone-Branch: org.NetBSD.atf.src

--- tests/atf/atf-c/h_lib.c
+++ tests/atf/atf-c/h_lib.c
@@ -60,11 +60,12 @@ build_check_c_o(const atf_tc_t *tc, const char *sfile, const char *failmsg)
                             atf_tc_get_config_var(tc, "srcdir"), sfile));
 
     RE(atf_check_build_c_o(atf_fs_path_cstring(&path), "test.o", optargs, &success));
-    if (!success)
-        atf_tc_fail(failmsg);
 
     atf_fs_path_fini(&path);
     atf_dynstr_fini(&iflag);
+
+    if (!success)
+        atf_tc_fail(failmsg);
 }
 
 bool
