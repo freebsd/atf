ad4ca0cce991ef2a12933068d1bbe6b71f418dba
Julio Merino <jmmv@NetBSD.org>
2009-07-13 05:07:40 +0000

    Make throw_atf_error throw accurate types
    Rework the way throw_atf_error works by making it raise different exceptions
    for different atf_error_t types.  libc_error is raised as system_error and
    no_memory_error is raised as bad_alloc.  We probably should standarize the
    names and bring the two libraries up to feature parity, as some errors are
    still unknown by the C++ binding.
    
    Monotone-Parent: e75e933b93a58edf6cc49167195ba6478a2fc189
    Monotone-Revision: 74476e74ea43b502318dbe66afb33a90539c2a5c
    
    Monotone-Author: jmmv@NetBSD.org
    Monotone-Date: 2009-07-13T05:07:40
    Monotone-Branch: org.NetBSD.atf.src

--- tests/atf/atf-c++/t_exceptions.cpp
+++ tests/atf/atf-c++/t_exceptions.cpp
@@ -27,9 +27,111 @@
 // IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
+extern "C" {
+#include "atf-c/error.h"
+}
+
+#include "atf-c++/exceptions.hpp"
 #include "atf-c++/macros.hpp"
+#include "atf-c++/sanity.hpp"
 
 #include "h_lib.hpp"
+
+// ------------------------------------------------------------------------
+// The "test" error.
+// ------------------------------------------------------------------------
+
+extern "C" {
+
+struct test_error_data {
+    const char* m_msg;
+};
+typedef struct test_error_data test_error_data_t;
+
+static
+void
+test_format(const atf_error_t err, char *buf, size_t buflen)
+{
+    const test_error_data_t* data;
+
+    PRE(atf_error_is(err, "test"));
+
+    data = static_cast< const test_error_data_t * >(atf_error_data(err));
+    snprintf(buf, buflen, "Message: %s", data->m_msg);
+}
+
+static
+atf_error_t
+test_error(const char* msg)
+{
+    atf_error_t err;
+    test_error_data_t data;
+
+    data.m_msg = msg;
+
+    err = atf_error_new("test", &data, sizeof(data), test_format);
+
+    return err;
+}
+
+} // extern
+
+// ------------------------------------------------------------------------
+// Tests cases for the free functions.
+// ------------------------------------------------------------------------
+
+ATF_TEST_CASE(throw_atf_error_libc);
+ATF_TEST_CASE_HEAD(throw_atf_error_libc)
+{
+    set_md_var("descr", "Tests the throw_atf_error function when raising "
+               "a libc error");
+}
+ATF_TEST_CASE_BODY(throw_atf_error_libc)
+{
+    try {
+        atf::throw_atf_error(atf_libc_error(1, "System error 1"));
+    } catch (const atf::system_error& e) {
+        ATF_CHECK(e.code() == 1);
+        ATF_CHECK(std::string(e.what()).find("System error 1") !=
+                  std::string::npos);
+    } catch (const std::exception& e) {
+        ATF_FAIL(std::string("Got unexpected exception: ") + e.what());
+    }
+}
+
+ATF_TEST_CASE(throw_atf_error_no_memory);
+ATF_TEST_CASE_HEAD(throw_atf_error_no_memory)
+{
+    set_md_var("descr", "Tests the throw_atf_error function when raising "
+               "a no_memory error");
+}
+ATF_TEST_CASE_BODY(throw_atf_error_no_memory)
+{
+    try {
+        atf::throw_atf_error(atf_no_memory_error());
+    } catch (const std::bad_alloc& e) {
+    } catch (const std::exception& e) {
+        ATF_FAIL(std::string("Got unexpected exception: ") + e.what());
+    }
+}
+
+ATF_TEST_CASE(throw_atf_error_unknown);
+ATF_TEST_CASE_HEAD(throw_atf_error_unknown)
+{
+    set_md_var("descr", "Tests the throw_atf_error function when raising "
+               "an unknown error");
+}
+ATF_TEST_CASE_BODY(throw_atf_error_unknown)
+{
+    try {
+        atf::throw_atf_error(test_error("The message"));
+    } catch (const std::runtime_error& e) {
+        const std::string msg = e.what();
+        ATF_CHECK(msg.find("The message") != std::string::npos);
+    } catch (const std::exception& e) {
+        ATF_FAIL(std::string("Got unexpected exception: ") + e.what());
+    }
+}
 
 // ------------------------------------------------------------------------
 // Tests cases for the header file.
@@ -43,6 +145,11 @@ HEADER_TC(include, "exceptions.hpp", "d_include_exceptions_hpp.cpp");
 
 ATF_INIT_TEST_CASES(tcs)
 {
+    // Add the test cases for the free functions.
+    ATF_ADD_TEST_CASE(tcs, throw_atf_error_libc);
+    ATF_ADD_TEST_CASE(tcs, throw_atf_error_no_memory);
+    ATF_ADD_TEST_CASE(tcs, throw_atf_error_unknown);
+
     // Add the test cases for the header file.
     ATF_ADD_TEST_CASE(tcs, include);
 }
