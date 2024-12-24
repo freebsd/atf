127c0c707578862ae38d9af0deee10fe91263784
Enji Cooper <ngie@FreeBSD.org>
2024-12-24 00:54:13 -0800

    atf-check: temp_file class minor improvements
    Make the namespace for `remove(..)` explicitly `atf::fs`.
    
    Signed-off-by: Enji Cooper <ngie@FreeBSD.org>

--- atf-sh/atf-check.cpp
+++ atf-sh/atf-check.cpp
@@ -144,7 +144,7 @@ public:
     {
         close();
         try {
-            remove(*m_path);
+            atf::fs::remove(*m_path);
         } catch (const atf::system_error&) {
             // Ignore deletion errors.
         }
