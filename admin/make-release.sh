54cf4f9957e444de4dc749a2db16c5f7b4faaf9e
Enji Cooper <ngie@FreeBSD.org>
2024-12-28 09:15:45 -0800

    fixup! admin/make-release.sh: automate release creation

--- admin/make-release.sh
+++ admin/make-release.sh
@@ -1,41 +1,31 @@
 #!/bin/sh
 #
-# Create a release from `ref`.
+# Create release artifacts from a release tag.
 #
 # Example:
 # 	./admin/make-release.sh atf-0.23
 
 set -eux
 
-ref=$1
+tag=$1
 
 cd "$(dirname "$(dirname "$0")")"
 
 mkdir -p releases
 release_root=$(realpath releases)
 
-release_dir="${release_root}/${ref}"
-release_artifact="${release_root}/${ref}.tar.gz"
+release_dir="${release_root}/${tag}"
+release_artifact="${release_root}/${tag}.tar.gz"
 
 rm -Rf "${release_dir}"
 mkdir -p "${release_dir}"
-git archive "${ref}" | tar xzvf - -C "${release_dir}"
+git archive "${tag}" | tar xzvf - -C "${release_dir}"
 cd "${release_dir}"
 autoreconf -is
 cd "${release_root}"
 bsdtar \
-    --exclude '*/.cirrus.yml' \
-    --exclude '*/.github/*' \
-    --exclude '*/.gitignore' \
-    --exclude '*/.travis.yml' \
-    --exclude '*/admin*' \
-    --exclude '*/autom4te.cache/*' \
-    --exclude '*/m4/libtool.m4' \
-    --exclude '*/m4/ltoptions.m4' \
-    --exclude '*/m4/ltsugar.m4' \
-    --exclude '*/m4/ltversion.m4' \
-    --exclude '*/m4/lt~obsolete.m4' \
+    --exclude-from=${release_dir}/admin/make-release.exclude \
     --uname "" --gname "" \
     --uid 0 --gid 0 \
-    -cvpzf "${release_artifact}" "${ref}"
+    -cvpzf "${release_artifact}" "${tag}"
 sha256 "${release_artifact##*/}" > "${release_artifact}.sha256"
