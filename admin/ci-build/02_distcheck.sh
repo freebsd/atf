cf0b307b1d5193e71fbd3e445707f1e6b087ecd4
Enji Cooper <ngie@FreeBSD.org>
2025-12-29 13:43:18 -0800

    CI workflow cleanup
    ci-build.sh-related changes:
    - Split up ci-build.sh into separate "steps" so the logic can be shared
      between Cirrus CI and GHA. As of today there are just 2 steps:
        - configure
        - distcheck
    - Set variables that can be overridden in the environment to sane
      default values to support `set -eu` and to make the code
      self-documenting.
    - Apply `set -u` to catch undefined variables as part of the script
      runtime.
    - Remove i386 support: it's effectively a dead 32-bit platform now which
      is not tested on a regular basis.
    
    In the future the style checks will be split off of `make distcheck`,
    i.e., once pre-commit integration is done.
    
    GHA-specific changes:
    
    - Make the file yamllint clean.
    - Skip the job summarization step.
        - The high-level summary provided by GHA each job is sufficient.
    - Archive the test artifacts on completion so the GHA steps can be
      debugged.
    - Set the environment with `env` instead of via a separate
      step with an undocumented interface, as `env` is the documented
      interface for setting environment.
    
    Signed-off-by: Enji Cooper <ngie@FreeBSD.org>

---
+++ admin/ci-build/02_distcheck.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# Step 2: run `make distcheck`.
+#
+# `make distcheck` builds the tests, runs them, and subsequently performs a
+# style check on the code.
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+set -eux
+
+: "${AS_ROOT=no}"
+: "${CC=cc}"
+: "${CXX=c++}"
+: "${EXTRA_DISTCHECK_CONFIGURE_ARGS=}"
+
+NPROC=$(nproc 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
+
+f=
+f="${f} ATF_BUILD_CC='${CC}'"
+f="${f} ATF_BUILD_CXX='${CXX}'"
+if [ -e "$(dirname "$(dirname "$0")")/.git" ]; then
+    f="${f} --enable-developer"
+fi
+if [ -n "${EXTRA_DISTCONFIGURE_ARGS:-}" ]; then
+    f="${f} ${EXTRA_DISTCONFIGURE_ARGS}"
+fi
+
+kyua_conf="$(realpath "$(mktemp kyua-XXXXXXXX.conf)")"
+trap 'rm -f "${kyua_conf}"' EXIT INT TERM
+
+sudo=
+
+cat >"${kyua_conf}" <<EOF
+syntax(2)
+
+unprivileged_user = 'nobody'
+EOF
+
+if [ "${AS_ROOT}" = yes ]; then
+    sudo="sudo -H"
+fi
+${sudo} env PATH="${PATH}" make distcheck -j"${NPROC}" \
+    DISTCHECK_CONFIGURE_FLAGS="${f}" \
+    KYUA_TEST_CONFIG_FILE="${kyua_conf}"
+
+# vim: syntax=sh:expandtab:shiftwidth=4:softtabstop=4
