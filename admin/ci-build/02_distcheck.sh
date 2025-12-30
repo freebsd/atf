freebsd/atf

commit cf0b307b1d5193e71fbd3e445707f1e6b087ecd4

CI workflow cleanup
ci-build.sh-related changes:
- Split up ci-build.sh into separate "steps" so the logic can be shared
  between Cirrus CI and GHA. As of today there are just 2 steps:
    - configure
    - distcheck
- Set variables that can be overridden in the environment to sane
  default values to support `set -eu` and to make the code
  self-documenting.
- Apply `set -u` to catch undefined variables as part of the script
  runtime.
- Remove i386 support: it's effectively a dead 32-bit platform now which
  is not tested on a regular basis.

In the future the style checks will be split off of `make distcheck`,
i.e., once pre-commit integration is done.

GHA-specific changes:

- Make the file yamllint clean.
- Skip the job summarization step.
    - The high-level summary provided by GHA each job is sufficient.
- Archive the test artifacts on completion so the GHA steps can be
  debugged.
- Set the environment with `env` instead of via a separate
  step with an undocumented interface, as `env` is the documented
  interface for setting environment.

Signed-off-by: Enji Cooper <ngie@FreeBSD.org>

.cirrus.yml
@@ -1,9 +1,14 @@
+---
+
 env:
   CIRRUS_CLONE_DEPTH: 1
   ARCH: amd64
 
 task:
+  timeout_in: 30m
   matrix:
+    # yamllint disable rule:comments
+    #
     # ngie: as of 2025-12-28 the 16.0-CURRENT supplied image on GCP does not
     # boot (gets stuck trying to find the bootdisk). Disable it so Cirrus CI
     # actually completes instead of getting stuck forever.
@@ -11,9 +16,12 @@ task:
     # See https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=292009 for more
     # details.
     #
+    #
     #- name: 16.0-CURRENT
     #  freebsd_instance:
     #    image_family: freebsd-16-0-snap
+    #
+    # yamllint enable rule:comments
     - name: 15-STABLE
       freebsd_instance:
         image_family: freebsd-15-0-amd64-ufs-snap
@@ -27,11 +35,17 @@ task:
       freebsd_instance:
         image_family: freebsd-14-3
   install_script:
+    # yamllint disable-line rule:line-length
     - sed -i.bak -e 's,pkg+http://pkg.FreeBSD.org/\${ABI}/quarterly,pkg+http://pkg.FreeBSD.org/\${ABI}/latest,' /etc/pkg/FreeBSD.conf
     - ASSUME_ALWAYS_YES=yes pkg bootstrap -f
     - pkg install -y autoconf automake libtool kyua pkgconf
   script:
-    - env JUNIT_OUTPUT=$(pwd)/test-results.xml ./admin/ci-build.sh
+    - ./admin/ci-build/01_configure.sh
+    - env JUNIT_OUTPUT="$(pwd)/test-results.xml"
+          ./admin/ci-build/02_distcheck.sh
+  on_failure:
+    dump_distcheck_logs:
+      - cat atf-*/_build/sub/config.log
   always:
     junit_artifacts:
       path: "test-results.xml"

.github/workflows/build.yaml
@@ -13,7 +13,7 @@
 #   * latest clang from brew (system provided clang lacks sanitizers).
 #   * ld from system
 name: build
-on:
+on:  # yamllint disable-line rule:truthy
     pull_request:
         branches:
             - master
@@ -26,6 +26,18 @@ permissions:
 jobs:
     build:
         name: build ${{ matrix.build-os }} ${{ matrix.compiler }}
+        env:
+            AR: ${{ matrix.llvm-bindir }}/llvm-ar
+            CC: ${{ matrix.llvm-bindir }}/clang
+            CPP: ${{ matrix.llvm-bindir }}/clang-cpp
+            CXX: ${{ matrix.llvm-bindir }}/clang++
+            NM: ${{ matrix.llvm-bindir }}/llvm-nm
+            RANLIB: ${{ matrix.llvm-bindir }}/llvm-ranlib
+            OBJDUMP: ${{ matrix.llvm-bindir }}/llvm-objdump
+            STRIP: ${{ matrix.llvm-bindir }}/llvm-strip
+            ATF_SRCDIR: ${{ github.workspace }}/src
+            ATF_OBJDIR: ${{ github.workspace }}/src
+            JOB_NAME: ${{ matrix.build-os }} ${{ matrix.compiler }}
         runs-on: "${{ matrix.build-os }}"
         strategy:
             fail-fast: false
@@ -71,61 +83,47 @@ jobs:
               uses: actions/checkout@v4
               with:
                   path: src
-            - name: Setup Environment
-              run: |
-                  echo "AR=${{ matrix.llvm-bindir }}/llvm-ar" >> "${GITHUB_ENV}"
-                  echo "CC=${{ matrix.llvm-bindir }}/clang" >> "${GITHUB_ENV}"
-                  echo "CPP=${{ matrix.llvm-bindir }}/clang-cpp" >> "${GITHUB_ENV}"
-                  echo "CXX=${{ matrix.llvm-bindir }}/clang++" >> "${GITHUB_ENV}"
-                  echo "NM=${{ matrix.llvm-bindir }}/llvm-nm" >> "${GITHUB_ENV}"
-                  echo "PKG_CONFIG_PATH=${PKG_CONFIG_PATH}" >> "${GITHUB_ENV}"
-                  echo "RANLIB=${{ matrix.llvm-bindir }}/llvm-ranlib" >> "${GITHUB_ENV}"
-                  echo "OBJDUMP=${{ matrix.llvm-bindir }}/llvm-objdump" >> "${GITHUB_ENV}"
-                  echo "STRIP=${{ matrix.llvm-bindir }}/llvm-strip" >> "${GITHUB_ENV}"
-                  echo "ATF_SRCDIR=${GITHUB_WORKSPACE}/src" >> "${GITHUB_ENV}"
-                  echo "ATF_OBJDIR=${GITHUB_WORKSPACE}/obj" >> "${GITHUB_ENV}"
-                  echo "ATF_PREFIX=${GITHUB_WORKSPACE}/installed" >> "${GITHUB_ENV}"
-                  echo "NPROC=`getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1`" >> "${GITHUB_ENV}"
             - name: Sanity checks
               run: |
                   which -a kyua
-            - name: Build ATF
+            - name: Build
               run: |
-                  CFG_OPTS="--disable-dependency-tracking"
-                  CFG_OPTS="${CFG_OPTS} --prefix=${ATF_PREFIX}"
-                  echo "Building atf with ${CFG_OPTS}..."
                   echo "uname -a: $(uname -a)"
                   echo "uname -m: $(uname -m)"
                   echo "uname -p: $(uname -p)"
                   echo "Build environment:"
-                  cat "${GITHUB_ENV}"
                   kyua about | head -1
+
                   cd "${ATF_SRCDIR}"
-                  autoreconf -isv -I/usr/local/share/aclocal
-                  mkdir -p "${ATF_OBJDIR}"
-                  cd "${ATF_OBJDIR}"
-                  "${ATF_SRCDIR}/configure" ${CFG_OPTS}
-                  make -j${NPROC} all | tee make-all.log
-            - name: Install ATF
+                  ./admin/ci-build/01_configure.sh
+            - name: Distcheck (builds and tests)
               run: |
-                  cd "${ATF_OBJDIR}"
-                  make install
-            - name: Test ATF
+                  CFG_ARGS="--disable-dependency-tracking --enable-developer"
+                  cd "${ATF_SRCDIR}"
+                  env "EXTRA_DISTCONFIGURE_ARGS=${CFG_ARGS}" \
+                      ./admin/ci-build/02_distcheck.sh
+            - name: Gather/Archive Logs and Reports
               run: |
-                  cd "${ATF_OBJDIR}"
-                  make installcheck
-                  check_exit=$?
-                  if [ $check_exit -eq 0 ]; then
-                      echo "# ✅ All mandatory checks passed" >> $GITHUB_STEP_SUMMARY
-                  else
-                      echo "# ❌ Some checks failed" >> $GITHUB_STEP_SUMMARY
-                      echo "::error file=.github/workflows/build.yaml,line=173,endLine=173,title=Checks failed!::checks failed"
-                  fi
-                  cd "${ATF_PREFIX}/tests/atf"
-                  kyua report-html --output ${ATF_OBJDIR}/html # produces html subdirectory
-                  # Include the plaintext and JUnit reports.
-                  kyua report --verbose --results-filter=xfail,broken,failed > ${ATF_OBJDIR}/html/test-reportfailed.txt
-                  kyua report-junit --output ${ATF_OBJDIR}/html/test-reportfailed.xml
-                  # Include the kyua log.
-                  cp -a ~/.kyua/logs ${ATF_OBJDIR}/html/
-                  exit $check_exit
+                  REPORTS_DIR="${ATF_OBJDIR}/reports"
+                  mkdir -p "${REPORTS_DIR}"
+                  (
+                    cd "${REPORTS_DIR}"
+                    mkdir -p build_logs
+                    cp -a "${ATF_SRCDIR}"/atf-*/_build/sub/*.log build_logs/
+                    # Include the raw kyua logs.
+                    cp -a ~/.kyua/logs kyua_logs
+                  )
+                  # Create an archive of the reports.
+                  job_name_encoded=$(echo "${JOB_NAME}" | tr ' ' '_')
+                  tar -cvf "atf-test-reports-${job_name_encoded}.tar" \
+                      -C "${REPORTS_DIR}" .
+              if: ${{ ! cancelled() }}
+            - name: Archive Build Artifacts
+              uses: actions/upload-artifact@v4
+              with:
+                  name: Test Report for ${{ env.JOB_NAME }}
+                  path: atf-test-reports-*.tar
+                  compression-level: 0
+                  retention-days: 10
+                  overwrite: true
+              if: ${{ ! cancelled() }}

admin/ci-build/02_distcheck.sh
@@ -1,0 +1,47 @@
+#!/bin/sh
+#
+# Step 2: run `make distcheck`.
+#
+# `make distcheck` builds the tests, runs them, and subsequently performs a
+# style check on the code.
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+set -eux
+
+: "${AS_ROOT=no}"
+: "${CC=cc}"
+: "${CXX=c++}"
+: "${EXTRA_DISTCHECK_CONFIGURE_ARGS=}"
+
+NPROC=$(nproc 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
+
+f=
+f="${f} ATF_BUILD_CC='${CC}'"
+f="${f} ATF_BUILD_CXX='${CXX}'"
+if [ -e "$(dirname "$(dirname "$0")")/.git" ]; then
+    f="${f} --enable-developer"
+fi
+if [ -n "${EXTRA_DISTCONFIGURE_ARGS:-}" ]; then
+    f="${f} ${EXTRA_DISTCONFIGURE_ARGS}"
+fi
+
+kyua_conf="$(realpath "$(mktemp kyua-XXXXXXXX.conf)")"
+trap 'rm -f "${kyua_conf}"' EXIT INT TERM
+
+sudo=
+
+cat >"${kyua_conf}" <<EOF
+syntax(2)
+
+unprivileged_user = 'nobody'
+EOF
+
+if [ "${AS_ROOT}" = yes ]; then
+    sudo="sudo -H"
+fi
+${sudo} env PATH="${PATH}" make distcheck -j"${NPROC}" \
+    DISTCHECK_CONFIGURE_FLAGS="${f}" \
+    KYUA_TEST_CONFIG_FILE="${kyua_conf}"
+
+# vim: syntax=sh:expandtab:shiftwidth=4:softtabstop=4

admin/ci-build.sh
@@ -1,78 +1,0 @@
-#! /bin/sh
-# Copyright 2014 Google Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-# * Redistributions of source code must retain the above copyright
-#   notice, this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimer in the
-#   documentation and/or other materials provided with the distribution.
-# * Neither the name of Google Inc. nor the names of its contributors
-#   may be used to endorse or promote products derived from this software
-#   without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-set -e -x
-
-if [ -d /usr/local/share/aclocal ]; then
-    autoreconf -isv -I/usr/local/share/aclocal
-else
-    autoreconf -isv
-fi
-
-ret=0
-./configure || ret=${?}
-if [ ${ret} -ne 0 ]; then
-    cat config.log || true
-    exit ${ret}
-fi
-
-archflags=
-[ "${ARCH?}" != i386 ] || archflags=-m32
-
-f=
-
-if [ -n "${archflags}" ]; then
-    CC=${CC-"cc"}
-    CXX=${CXX-"c++"}
-
-    f="${f} ATF_BUILD_CC='${CC} ${archflags}'"
-    f="${f} ATF_BUILD_CXX='${CXX} ${archflags}'"
-    f="${f} CFLAGS='${archflags}'"
-    f="${f} CXXFLAGS='${archflags}'"
-    f="${f} LDFLAGS='${archflags}'"
-fi
-
-if [ "${AS_ROOT:-no}" = yes ]; then
-    cat >root-kyua.conf <<EOF
-syntax(2)
-unprivileged_user = 'nobody'
-EOF
-    ret=0
-    sudo -H PATH="${PATH}" make distcheck DISTCHECK_CONFIGURE_FLAGS="${f}" \
-        KYUA_TEST_CONFIG_FILE="$(pwd)/root-kyua.conf" || ret=${?}
-else
-    ret=0
-    make distcheck DISTCHECK_CONFIGURE_FLAGS="${f}" || ret=${?}
-fi
-if [ ${ret} -ne 0 ]; then
-    cat atf-*/_build/sub/config.log || true
-    exit ${ret}
-fi
-
-# vim: syntax=sh:expandtab:shiftwidth=4:softtabstop=4

admin/ci-build/01_configure.sh
@@ -1,0 +1,23 @@
+#!/bin/sh
+#
+# Step 1: run autoconf/configure so the remaining of the build/test steps can
+# be completed.
+#
+# Splitting off this step allows any configure issues to be found quickly and
+# triaged more effectively.
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+autoreconf_args="-isv"
+if [ -d /usr/local/share/aclocal ]; then
+    autoreconf_args="${autoreconf_args} -I/usr/local/share/aclocal"
+fi
+# shellcheck disable=SC2086
+autoreconf ${autoreconf_args}
+
+if ! ./configure; then
+    cat config.log || true
+    exit 1
+fi
+
+# vim: syntax=sh:expandtab:shiftwidth=4:softtabstop=4

commit e4f7b6a89bab03a17040e3bcf81bc58c7daf3276

CI workflow cleanup fixes (part 2)
- Fix typos related to `EXTRA_DISTCHECK_CONFIGURE_ARGS`.
- Fix typo in specifying the temporary kyua config to avoid possible
  collisions with the filenames.

Fixes:  cf0b307b1d5193e ("CI workflow cleanup")
Signed-off-by: Enji Cooper <ngie@FreeBSD.org>

.cirrus.yml
@@ -41,7 +41,8 @@ task:
     - pkg install -y autoconf automake libtool kyua pkgconf
   script:
     - ./admin/ci-build/01_configure.sh
-    - env JUNIT_OUTPUT="$(pwd)/test-results.xml"
+    - env EXTRA_DISTCHECK_CONFIGURE_ARGS="--enable-developer"
+          JUNIT_OUTPUT="$(pwd)/test-results.xml"
           ./admin/ci-build/02_distcheck.sh
   on_failure:
     dump_distcheck_logs:

.github/workflows/build.yaml
@@ -100,7 +100,7 @@ jobs:
               run: |
                   CFG_ARGS="--disable-dependency-tracking --enable-developer"
                   cd "${ATF_SRCDIR}"
-                  env "EXTRA_DISTCONFIGURE_ARGS=${CFG_ARGS}" \
+                  env "EXTRA_DISTCHECK_CONFIGURE_ARGS=${CFG_ARGS}" \
                       ./admin/ci-build/02_distcheck.sh
             - name: Gather/Archive Logs and Reports
               run: |

admin/ci-build/02_distcheck.sh
@@ -22,11 +22,11 @@ f="${f} ATF_BUILD_CXX='${CXX}'"
 if [ -e "$(dirname "$(dirname "$0")")/.git" ]; then
     f="${f} --enable-developer"
 fi
-if [ -n "${EXTRA_DISTCONFIGURE_ARGS:-}" ]; then
-    f="${f} ${EXTRA_DISTCONFIGURE_ARGS}"
+if [ -n "${EXTRA_DISTCHECK_CONFIGURE_ARGS:-}" ]; then
+    f="${f} ${EXTRA_DISTCHECK_CONFIGURE_ARGS}"
 fi
 
-kyua_conf="$(realpath "$(mktemp kyua-XXXXXXXX.conf)")"
+kyua_conf="$(realpath "$(mktemp kyuaconf-XXXXXXXX)")"
 trap 'rm -f "${kyua_conf}"' EXIT INT TERM
 
 sudo=